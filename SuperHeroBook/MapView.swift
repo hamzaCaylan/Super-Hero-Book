1abf7389b475e52b43b46e4b94cc4df2dd399b49
hamzaçaylan <hamzacaylan021@gmail.come>
2023-06-08 12:09:21 +0300

    MapKit kullanım şekilleri

--- SuperHeroBook/MapView.swift
+++ SuperHeroBook/MapView.swift
@@ -8,16 +8,33 @@
 import SwiftUI
 import MapKit
 
-struct MapView: View {
+/*struct MapView: View {
     
     var body: some View {
-        @State var region = MKCoordinateRegion(center: SuperHeroDizisi[0].coordinateLocal, span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+        @State var region = MKCoordinateRegion(center: superKahramanDizisi[0].koordinatLokasyonu,
+                                               span: MKCoordinateSpan(latitudeDelta: 0.35, longitudeDelta: 0.35))
         Map(coordinateRegion: $region)
+    }
+}*/
+
+struct MapView: UIViewRepresentable {
+    var kordinat : CLLocationCoordinate2D
+    
+    func updateUIView(_ uiView: MKMapView , context: Context) {
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.35, longitudeDelta: 0.45)
+        let region = MKCoordinateRegion(center: kordinat, span: span)
+        uiView.setRegion(region, animated: true)
+         
+    }
+    
+    func makeUIView(context: Context) -> MKMapView{
+        MKMapView(frame: .zero)
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(kordinat: superKahramanDizisi[0].koordinatLokasyonu )
     }
 }
