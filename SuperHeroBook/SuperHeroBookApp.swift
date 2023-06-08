4ec683a28d8635ba3318448afcb05379fdb68efa
hamzaçaylan <hamzacaylan021@gmail.come>
2023-06-08 11:13:29 +0300

    Initial Commit

---
+++ SuperHeroBook/SuperHeroBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroBookApp.swift
+//  SuperHeroBook
+//
+//  Created by Hamza Caylan on 8.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
